0036aa698540275f2c79304300feec57a4073065
light885 <lenya074@gmail.com>
2021-10-14 13:46:05 +0200

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,18 @@
 $(function () {
-  
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000
-  })
+  });
+
+  $('.star').rateYo({
+    starWidth: "16px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
+
 });
